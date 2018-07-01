5b85bc384a7b3566c6db8b7c313586bde2fb9530
Raulpatle <therahulpatle@gmail.com>
2018-07-01 13:55:15 +0530

    checking in login po and tc file

--- src/com/yantra/product/parabank/po/PO_LoginPage.java
+++ src/com/yantra/product/parabank/po/PO_LoginPage.java
@@ -1,5 +1,40 @@
 package com.yantra.product.parabank.po;
 
-public class PO_LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
-}
+public class PO_LoginPage {
+	WebDriver driver;
+	 @FindBy(how=How.XPATH,using="//input[@name='username']")
+	 private WebElement txtbx_UserName;
+
+	 @FindBy(how=How.XPATH,using="//input[@name='password']")
+	 private WebElement txtbx_Password;
+	 
+	 @FindBy(how=How.XPATH,using="//input[@class='button']")
+	private WebElement btn_Login;
+	 
+	 public void SetUsername(String Username) {
+		 txtbx_UserName.sendKeys("john");
+	 }
+	 public void SetPassword(String password) 
+	 {txtbx_Password.sendKeys("demo");
+		 
+	 }
+	 public void SetSubmit()
+	 {btn_Login.click();
+		 
+	 }
+	 public void POM(WebDriver d)
+	 {
+		 this.driver= d;
+	 }
+
+
+		}
+
+
--- src/com/yantra/product/parabank/tc/TC_LoginPage.java
+++ src/com/yantra/product/parabank/tc/TC_LoginPage.java
@@ -1,5 +1,27 @@
 package com.yantra.product.parabank.tc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.yantra.product.parabank.po.PO_LoginPage;
+
 public class TC_LoginPage {
+	public void t_01_validate_parabank_login(){
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", ".//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("http://parabank.parasoft.com/parabank/index.htm");
+		
+		String var = "Akash";
+		
+		PO_LoginPage loginpage=PageFactory.initElements(driver,PO_LoginPage.class);
+		loginpage.SetUsername("john");
+		loginpage.SetPassword("demo");
+		loginpage.SetSubmit();
+		
+	}
 
 }
