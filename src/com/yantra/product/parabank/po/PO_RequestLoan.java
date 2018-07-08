a78abee10a5ade945ad44d1e795e75651fc7d58b
SnehaDhote <snehadhote777@gmail.com>
2018-07-08 10:22:07 +0530

    adding sneha changes-PO request loan file

--- src/com/yantra/product/parabank/po/PO_RequestLoan.java
+++ src/com/yantra/product/parabank/po/PO_RequestLoan.java
@@ -1,5 +1,32 @@
 package com.yantra.product.parabank.po;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PO_RequestLoan {
+	WebDriver driver;
+	@FindBy(id="amount")
+	private WebElement  loanamount;
+	@FindBy(id="downPayment")
+	private WebElement downpayment;
+	@FindBy(id="fromAccountId")
+	private WebElement fromaccount;
+	@FindBy(xpath="//input[@type='submit']")
+	private WebElement apply;
+	
+	private void Setloanamount(String a)
+	{
+		 loanamount.sendKeys("loanamount");
+	 }
+	
+	private void Setdownpayment(String a)
+	{
+		 downpayment.sendKeys("downpayment");
+	 }
+	private void Setdfromaccount(String accountno)
+	{
+		 fromaccount.sendKeys("accountno");
+	 }
 
 }
