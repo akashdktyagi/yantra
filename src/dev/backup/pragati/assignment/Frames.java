0ca6164eeba3bfbe4dd11467af9b6112572437cd
Pragati789 <pragati.shakuwar@gmail.com>
2018-09-01 11:34:58 +0530

    Change code

--- src/dev/backup/pragati/assignment/Frames.java
+++ src/dev/backup/pragati/assignment/Frames.java
@@ -24,7 +24,16 @@ public class Frames {
 	     WebElement dragg=driver.findElement(By.id("draggable"));
 	     WebElement drop=driver.findElement(By.id("droppable"));
 	     action.dragAndDrop(dragg, drop).perform();
-	
+
+	     
+	     //resizable
+	     
+	     //driver.get("https://jqueryui.com/resizable/");
+	    // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	    // WebElement resize=driver.findElement(By.xpath("//div[@id='resizable']/div[3]"));
+	     //ction.clickAndHold().moveByOffset(20, 50).release().perform();
+	    
+	     
 
 }
 }
