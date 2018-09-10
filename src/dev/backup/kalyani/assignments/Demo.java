f0bf606bcb02ae0163d4b9fbeaa68d13f8d3aa87
kalyani9604 <kalyanigaikwad5192@gmail.com>
2018-09-10 11:33:09 +0530

    updated

--- src/dev/backup/kalyani/assignments/Demo.java
+++ src/dev/backup/kalyani/assignments/Demo.java
@@ -2,7 +2,9 @@ package dev.backup.kalyani.assignments;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		//output on console
 		System.out.println("Hello World");
 
 	}
