ab12dbea02b903109bfd3031f641c7d5d35a1e79
Ravikanherkar <ravikanherkar431@gmail.com>
2018-10-05 15:29:49 +0530

    dsfahd

---
+++ src/dev/backup/ravi/assignment/java/A.java
@@ -0,0 +1,18 @@
+package dev.backup.ravi.assignment.java;
+
+public class A {
+	
+	public A(){
+		System.out.println("parent class--constru");
+		
+	}
+	public A(int i){
+		System.out.println("parent class cons with value i="+i);
+	}
+	public A(int i,int j){
+
+		System.out.println("parent class cons with value i="+i);
+		System.out.println("parent class cons with value j="+j);
+
+}}
+
